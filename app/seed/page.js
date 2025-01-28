0b79d87cfa0d87978887febd8226c04fde0ae1cb
Josep Tormo <jostorgar4@alu.edu.gva.es>
2025-01-28 16:43:11 +0100

    .

--- app/seed/page.js
+++ app/seed/page.js
@@ -1,9 +1,9 @@
 import { sql } from "@vercel/postgres"
 
 export default async () => {
-    await sql`CREATE TABLE POSTS(id UUID DEFAULT uuid_generate_V4() PRIMARY KEY,
-    content TEXT,
-    url TEXT )`
+    // await sql`CREATE TABLE POSTS(id UUID DEFAULT uuid_generate_V4() PRIMARY KEY,
+    // content TEXT,
+    // url TEXT )`
 
     return <p>Database seed the guay</p>
 }
