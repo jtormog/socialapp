3901192f5137f47b70891efa59e1721d5ce51981
Josep Tormo García <joseptormogarcia@gmail.com>
2025-02-27 13:41:25 +0100

    .

--- app/create/page.js
+++ app/create/page.js
@@ -1,12 +1,40 @@
+'use client'
 import { createPost } from "../lib/actions"
 import ImageSelector from "../ui/imageSelector"
+import { useState } from "react"
 
 export default () => {
+    const [isSend, setIsSend] = useState(false)
+    
+    async function handleSend(e) {
+        e.preventDefault()
+        if (isSend) return;
+        try {
+            setIsSend(true)
+            const formData = new FormData(e.target)
+            await createPost(formData)
+        } catch (error) {
+            console.error('Error creating post:', error)
+            setIsSend(false)
+        }
+    }
+    
     return(
-        <form action={createPost} className="flex flex-col gap-8 items-center justify-center w-full" >
-            <input name="content" required className="w-full max-w-lg dark:bg-neutral-950 w-full outline-none" placeholder="¿Que estás pensando?"/>
-            <ImageSelector />
-            <input type="submit"/>
+        <form onSubmit={handleSend} className="flex flex-col gap-8 items-center justify-center w-full">
+            <input 
+                name="content" 
+                required 
+                disabled={isSend}
+                className="w-full max-w-lg dark:bg-neutral-950 outline-none disabled:opacity-50 disabled:cursor-not-allowed" 
+                placeholder="¿Que estás pensando?"
+            />
+            <ImageSelector/>
+            <input 
+                disabled={isSend} 
+                type="submit" 
+                value='Enviar imagen'
+                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-lg transition-colors disabled:bg-gray-400 disabled:cursor-not-allowed disabled:hover:bg-gray-400"
+            />
         </form>
     )
 }
